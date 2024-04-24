83ac58a8f622ccb2ce14fbc225fef7fddb19ae5c
Pegerto <pegerto@gmail.com>
2024-04-24 16:34:32 +0100

    Add GNM modes annotations (#35)
    Co-authored-by: Pegerto Fernandez <pegerto.fernandez@creditkarma.com>

---
+++ src/graphpro/util/modes.py
@@ -0,0 +1,12 @@
+from graphpro.model import AtomGroup
+import prody as pdy
+
+def compute_gnm_slow_modes(ag: AtomGroup, n_modes: int = 20):
+    pdy_ag = ag.to_prody()
+    calphas = pdy_ag.select('calpha')
+    gnm = pdy.GNM()
+
+    gnm.buildKirchhoff(calphas)
+    gnm.calcModes(n_modes)
+
+    return ([a.getResnum() for a in calphas], gnm.getEigvecs())
---
+++ test/graphpro/annnotations/modes_test.py
@@ -0,0 +1,30 @@
+import MDAnalysis as mda
+import torch
+
+from graphpro import md_analisys
+from graphpro.graphgen import ContactMap
+from graphpro.annotations import GNMSlowModes
+
+from MDAnalysis.tests.datafiles import PDB_small
+
+u1 = mda.Universe(PDB_small)
+
+def test_gnm_slow_modes_annotation():
+    G = md_analisys(u1).generate(ContactMap(cutoff=6), [GNMSlowModes(modes=3)])
+    assert len(G.nodes()) == 214
+    assert G.node_attr(0)['gnm_slow_mode_0'] == -0.038
+    assert G.node_attr(0)['gnm_slow_mode_1'] == -0.047
+    assert G.node_attr(0)['gnm_slow_mode_2'] == 0.074
+
+def test_gnm_slow_modes_is_present_for_all_nodes():
+     G = md_analisys(u1).generate(ContactMap(cutoff=6), [GNMSlowModes(modes=3)])
+     for n in G.nodes():
+          assert 'gnm_slow_mode_0' in G.node_attr(n)
+          assert 'gnm_slow_mode_1' in G.node_attr(n)
+
+def test_gnm_encoding():
+    G = md_analisys(u1).generate(ContactMap(cutoff=6), [GNMSlowModes(modes=3)])
+    data = G.to_data(node_encoders=[GNMSlowModes(modes=3)])
+
+    assert data.x.size() == (214, 3)
+    assert data.x.dtype == torch.float
