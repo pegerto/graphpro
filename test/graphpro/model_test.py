27845b18f6343a695c8ff139431a214246b9703f
Pegerto <pegerto@gmail.com>
2024-04-24 15:12:48 +0100

    enable prody integration (#34)
    Co-authored-by: Pegerto Fernandez <pegerto.fernandez@creditkarma.com>

--- test/graphpro/model_test.py
+++ test/graphpro/model_test.py
@@ -14,3 +14,29 @@ def test_model_from_mdanalysis_partial_occupancy():
     ag = MDAnalisysAtomGroup(FIVEHTC)
     assert(len(ag.c_alphas_residues(chain="C")) ==
            len(ag.c_alphas_positions(chain="C")))
+
+
+def test_transform_to_prody_contains_same_atoms():
+    ag = MDAnalisysAtomGroup(FIVEHTC)
+    pdAG = ag.to_prody()
+    assert(len(pdAG) == 3346)
+
+def test_transform_to_prody_cotains_atom_names():
+    ag = MDAnalisysAtomGroup(FIVEHTC)
+    pdAG = ag.to_prody()
+    assert(pdAG.getNames()[0] == 'N')
+
+def test_transform_to_prody_cotains_residue_names():
+    ag = MDAnalisysAtomGroup(FIVEHTC)
+    pdAG = ag.to_prody()
+    assert(pdAG.getResnames()[0] == 'GLY')
+
+def test_transform_to_prody_cotains_residue_number():
+    ag = MDAnalisysAtomGroup(FIVEHTC)
+    pdAG = ag.to_prody()
+    assert(pdAG.getResnums()[0] == 471)
+
+def test_transform_to_prody_contains_chain_id():
+    ag = MDAnalisysAtomGroup(FIVEHTC)
+    pdAG = ag.to_prody()
+    assert(pdAG.getChids()[0] == 'A')
