f8b6ab4866a59a34410ed4e683f2e556ce151dac
Pegerto Fernandez <pegerto.fernandez@creditkarma.com>
2025-02-28 14:51:48 +0000

    avoid rounding error test

--- test/graphpro/annnotations/energy_test.py
+++ test/graphpro/annnotations/energy_test.py
@@ -12,7 +12,7 @@ u1 = mda.Universe(PDB_small)
 def test_bt_potential_calculation():
     G = md_analisys(u1).generate(ContactMap(cutoff=6), [BTPotential()])
     assert len(G.nodes()) == 214
-    assert G.node_attr(0)['bt_potential'] == -702.0824887338479
+    assert round(G.node_attr(0)['bt_potential'],2) == -702.08
 
 def test_gnm_encoding():
     G = md_analisys(u1).generate(ContactMap(cutoff=6), [BTPotential()])
